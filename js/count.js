8aff4957f008125e34eae52578306b71271cd625
paul morin & john young <student@epicodus.com>
2015-06-26 12:20:13 -0700

    a little logic, not much

---
+++ js/count.js
@@ -0,0 +1,21 @@
+var count_up_by = function(digit) {
+  var count = 
+
+};
+
+// var up_to = number[0]
+// var multiple = number[1]
+
+
+
+
+
+
+$(document).ready(function(){
+  $("form#count_up_by").submit(function(event){
+    var number = parseInt($("input#number").val());
+    var multiple = parseInt($("input#multiple").val());
+    var result = count_up_by(digit);
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,10 @@
 describe('count_up_by', function(){
-  it("it counts up to a number", function(){
+  it("it counts up to a predetermined number", function(){
     expect(count_up_by("25")).to.equal("25");
   });
+
+  it("it counts up to the number by a multiple entered by a user", function(){
+    expect(count_up_by([5, 25])).to.equal([5,10,15,20,25]);
+  });
+
 });
